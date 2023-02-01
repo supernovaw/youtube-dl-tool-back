f507695edb7e03a55e0ab1b02513a436544722f9
supernovaw <ruszrj@gmail.com>
2023-02-01 07:20:07 -0800

    add allowed origin (CORS)

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import express, { Router } from "express";
+import cors from "cors";
 import auth from "./routes/auth";
 
 const baseRouter = Router();
@@ -6,7 +7,10 @@ baseRouter.use("/auth", auth);
 
 const port = process.env.PORT || 44393;
 const baseUrl = process.env.BASE_URL || "/youtube-dl-tool/api";
+const origin = process.env.ORIGIN || "http://localhost";
+
 const app = express();
+app.use(cors({ origin }));
 app.use(express.json());
 app.use(baseUrl, baseRouter);
 app.listen(port, () => console.log(`youtube-dl-tool-back :${port} started`));
